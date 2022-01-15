50438d2ca0a239f5823d21016dcc947bf4f61f07
fuyutarow <fuyutarow@gmail.com>
2022-01-15 20:19:29 +0900

    Fix addMemberList

--- src/App.tsx
+++ src/App.tsx
@@ -1,94 +1,139 @@
 import * as React from 'react';
 import { useState, useEffect } from 'react';
 import Container from '@material-ui/core/Container';
+import { Box, Button, TextField } from '@material-ui/core';
 
 import { name as appName, version } from '../package.json';
 
 import './App.css';
 import { Fork } from './models';
 import Table from './components/Table';
+import { sleep } from './utils';
 
-import { Button } from '@material-ui/core';
+const jumpUrl = (url: string) => {
+  chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+    chrome.tabs.sendMessage(
+      // @ts-ignore
+      tabs[0].id,
+      JSON.stringify({ action: 2, url }),
+      (res) => { }
+    );
+  });
+};
+
+const addMemberList = () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+    chrome.tabs.sendMessage(
+      // @ts-ignore
+      tabs[0].id,
+      JSON.stringify({ action: 1 }),
+      (res) => { }
+    );
+  });
+
+};
 
 const App: React.FC = () => {
-  const [currentUrl, setCurrentUrl] = useState<string | null>(null);
-  const [repo, setRepo] = useState<string|null>(null);
-  // const [repoUrl, setRepoUrl] = useState<string|null>(null);
-  const [forks, setForks] = useState<Fork[]>([]);
+  const [input, setInput] = useState('');
+  const [userNameList, setUserNameList] = useState<string[]>([]);
+  const [current, setCurrent] = useState('');
+  const [isFinished, setIsFinished] = useState(false);
 
   useEffect(() => {
-    chrome.tabs.query({ 'active': true, 'lastFocusedWindow': true }, tabs => {
-      const tab = tabs[0];
-      setCurrentUrl(tab.url ?? null);
-
-    });
-  }, [currentUrl]);
-
-  useEffect(() => {
-    if (currentUrl) {
-      const parser = new URL(currentUrl);
-      const repo = parser.pathname.split('/').slice(1, 3).join('/').replace(/\.git$/, '');
-      setRepo(repo);
-    }
-  }, [currentUrl, repo]);
-
-  useEffect(() => {
-    const unlisten = async () => {
-      const response = await fetch(
-        `https://api.github.com/repos/${repo}/forks?sort=stargazers&per_page=100`
-      );
-      // .catch(e => alert(e));
-      if (!response.ok) throw Error(response.statusText);
-      const data = await response.json();
-      console.log(data);
-
-      const forks = data.map((fork: any) => {
-        return {
-          ...fork,
-          ownerName: fork.owner ? fork.owner.login : 'Unknown',
-        } as Fork;
-      });
-
-      setForks(forks);
-    };
-    unlisten();
-  }, [repo, forks]);
+    const names = input.split(',').map(s => s.trim().replace('@', ''));
+    setUserNameList(names);
+  }, [input]);
 
   return (
-    <div className="App">
-      <Container className="App-container" style={{
-        backgroundColor: '#ECEFF4',
-        paddingTop: 20,
-        paddingBottom: 20,
+    <Container className="App-container" style={{
+      backgroundColor: '#ECEFF4',
+      paddingTop: 20,
+      paddingBottom: 20,
+      width: 600,
+    }}>
+      <Button
+        children="user list"
+        onClick={() => {
+          alert(userNameList);
+        }}
+      />
+      <TextField
+        id="text-field"
+        label="カンマ区切りでtwitter idを複数入力"
+        multiline
+        rows={4}
+        value={input}
+        onChange={ e => setInput(e.target.value)}
+      />
+      <Box style={{
+        color: 'black',
       }}>
-        <Button
-          children="btn1"
-          onClick={async() => {
-            chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-              chrome.tabs.sendMessage(
-                // @ts-ignore
-                tabs[0].id,
-                JSON.stringify({ action: 1 }),
-                (res) => { }
-              );
-            });
-          }}
-        />
-        <Button
-          children="btn2"
-          onClick={async() => {
-            chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-              chrome.tabs.sendMessage(
-                // @ts-ignore
-                tabs[0].id,
-                JSON.stringify({ action: 2 }),
-                (res) => { }
-              );
-            });
-          }}
-        />
-      </Container>
-    </div>
+        {isFinished ? 'DONE' : current}
+      </Box>
+      <Button
+        children="Go"
+        onClick={async () => {
+          for (const userName of userNameList) {
+            setCurrent(userName);
+            jumpUrl(`https://twitter.com/${userName}`);
+            await sleep(2e3);
+            addMemberList();
+            await sleep(4e3);
+          }
+          setIsFinished(true);
+        }}
+      />
+      <Button
+        children="btn1"
+        onClick={async () => {
+          chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+            chrome.tabs.sendMessage(
+              // @ts-ignore
+              tabs[0].id,
+              JSON.stringify({ action: 1 }),
+              (res) => { }
+            );
+          });
+        }}
+      />
+      <Button
+        children="btn2"
+        onClick={async () => {
+          chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+            chrome.tabs.sendMessage(
+              // @ts-ignore
+              tabs[0].id,
+              JSON.stringify({ action: 2, url: 'https://twitter.com/bonbonnice1' }),
+              (res) => { }
+            );
+          });
+        }}
+      />
+      <Button
+        children="btn1"
+        onClick={async () => {
+          chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+            chrome.tabs.sendMessage(
+              // @ts-ignore
+              tabs[0].id,
+              JSON.stringify({ action: 1 }),
+              (res) => { }
+            );
+          });
+        }}
+      />
+      <a
+        href={
+          'https://twitter.com/bonbonnice1'
+        }
+        onClick={(e: any) => {
+          if (e.target.href !== undefined) {
+            chrome.tabs.create({ url: e.target.href });
+          }
+        }}>
+          url
+      </a>
+    </Container>
   );
 };
 
--- src/content.tsx
+++ src/content.tsx
@@ -9,109 +9,54 @@ console.log('===========================================');
 console.log('Hello from content script', appName, version);
 console.log('===========================================');
 
-const Button1 = () => {
+const addMemberList = async () => {
+  await sleep(200);
+  const dots = document.querySelector(
+    '#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div > div.css-1dbjc4n.r-yfoy6g.r-18bvks7.r-1ljd8xs.r-13l2t4g.r-1phboty.r-1jgb5lz.r-11wrixw.r-61z16t.r-1ye8kvj.r-13qz1uu.r-184en5c > div > div:nth-child(2) > div > div > div:nth-child(1) > div > div.css-1dbjc4n.r-1habvwh.r-18u37iz.r-1w6e6rj.r-1wtj0ep > div.css-1dbjc4n.r-obd0qt.r-18u37iz.r-1w6e6rj.r-1h0z5md.r-dnmrzs > div.css-18t94o4.css-1dbjc4n.r-1niwhzg.r-sdzlij.r-1phboty.r-rs99b7.r-6gpygo.r-1kb76zh.r-2yi16.r-1qi8awa.r-1ny4l3l.r-o7ynqc.r-6416eg.r-lrvibr > div'
+  );
+  // @ts-ignore
+  dots?.click();
+
+  await sleep(200);
+  const item_list = document.querySelector(
+    '#layers > div.css-1dbjc4n.r-1p0dtai.r-1d2f490.r-105ug2t.r-u8s1d.r-zchlnj.r-ipm5af > div > div > div > div:nth-child(2) > div.css-1dbjc4n.r-yfoy6g.r-z2wwpe.r-xnswec.r-j2cz3j.r-1udh08x.r-u8s1d > div > div > div > a:nth-child(2)'
+  );
+  // @ts-ignore
+  item_list?.click();
+
+  await sleep(1000);
+  const node = document.querySelector(
+    '#layers > div:nth-child(2) > div > div > div > div > div > div.css-1dbjc4n.r-1awozwy.r-18u37iz.r-1pi2tsx.r-1777fci.r-1xcajam.r-ipm5af.r-g6jmlv > div.css-1dbjc4n.r-1867qdf.r-1wbh5a2.r-kwpbio.r-rsyp9y.r-1pjcn9w.r-1279nm1.r-htvplk.r-1udh08x > div > div > div > div:nth-child(2) > section > div > div > div:nth-child(2) > div > a'
+  );
+  console.log(node);
+  // @ts-ignore
+  node?.click();
+
+  await sleep(200);
+  const save = document.querySelector(
+    '#layers > div:nth-child(2) > div > div > div > div > div > div.css-1dbjc4n.r-1awozwy.r-18u37iz.r-1pi2tsx.r-1777fci.r-1xcajam.r-ipm5af.r-g6jmlv > div.css-1dbjc4n.r-1867qdf.r-1wbh5a2.r-kwpbio.r-rsyp9y.r-1pjcn9w.r-1279nm1.r-htvplk.r-1udh08x > div > div > div > div.css-1dbjc4n.r-gtdqiz.r-ipm5af.r-136ojw6 > div > div > div > div > div > div.css-1dbjc4n.r-obd0qt.r-1pz39u2.r-1777fci.r-15ysp7h.r-s8bhmr > div'
+  );
+  // @ts-ignore
+  save?.click();
+};
+
+const AddMemberListButton = () => {
   return (
     <button
-      onClick={async () => {
-        const dots = document.querySelector(
-          '#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div > div.css-1dbjc4n.r-yfoy6g.r-18bvks7.r-1ljd8xs.r-13l2t4g.r-1phboty.r-1jgb5lz.r-11wrixw.r-61z16t.r-1ye8kvj.r-13qz1uu.r-184en5c > div > div:nth-child(2) > div > div > div:nth-child(1) > div > div.css-1dbjc4n.r-1habvwh.r-18u37iz.r-1w6e6rj.r-1wtj0ep > div.css-1dbjc4n.r-obd0qt.r-18u37iz.r-1w6e6rj.r-1h0z5md.r-dnmrzs > div.css-18t94o4.css-1dbjc4n.r-1niwhzg.r-sdzlij.r-1phboty.r-rs99b7.r-6gpygo.r-1kb76zh.r-2yi16.r-1qi8awa.r-1ny4l3l.r-o7ynqc.r-6416eg.r-lrvibr > div'
-        );
-        // @ts-ignore
-        dots?.click();
-
-        await sleep(200);
-        const item_list = document.querySelector(
-          '#layers > div.css-1dbjc4n.r-1p0dtai.r-1d2f490.r-105ug2t.r-u8s1d.r-zchlnj.r-ipm5af > div > div > div > div:nth-child(2) > div.css-1dbjc4n.r-yfoy6g.r-z2wwpe.r-xnswec.r-j2cz3j.r-1udh08x.r-u8s1d > div > div > div > a:nth-child(2)'
-        );
-        // @ts-ignore
-        item_list?.click();
-
-        await sleep(1000);
-        const node = document.querySelector(
-          '#layers > div:nth-child(2) > div > div > div > div > div > div.css-1dbjc4n.r-1awozwy.r-18u37iz.r-1pi2tsx.r-1777fci.r-1xcajam.r-ipm5af.r-g6jmlv > div.css-1dbjc4n.r-1867qdf.r-1wbh5a2.r-kwpbio.r-rsyp9y.r-1pjcn9w.r-1279nm1.r-htvplk.r-1udh08x > div > div > div > div:nth-child(2) > section > div > div > div:nth-child(2) > div > a'
-        );
-        console.log(node);
-        // @ts-ignore
-        node?.click();
-
-        await sleep(200);
-        const save = document.querySelector(
-          '#layers > div:nth-child(2) > div > div > div > div > div > div.css-1dbjc4n.r-1awozwy.r-18u37iz.r-1pi2tsx.r-1777fci.r-1xcajam.r-ipm5af.r-g6jmlv > div.css-1dbjc4n.r-1867qdf.r-1wbh5a2.r-kwpbio.r-rsyp9y.r-1pjcn9w.r-1279nm1.r-htvplk.r-1udh08x > div > div > div > div.css-1dbjc4n.r-gtdqiz.r-ipm5af.r-136ojw6 > div > div > div > div > div > div.css-1dbjc4n.r-obd0qt.r-1pz39u2.r-1777fci.r-15ysp7h.r-s8bhmr > div'
-        );
-        // @ts-ignore
-        save?.click();
-      }}
+      onClick={addMemberList}
       children="add member list"
     />
   );
 };
 
-const Button2 = () => {
-  return (
-    <button
-      onClick={async () => {
-        const dots = document.querySelector(
-          '#layers > div.css-1dbjc4n.r-1p0dtai.r-1d2f490.r-105ug2t.r-u8s1d.r-zchlnj.r-ipm5af > div > div > div > div:nth-child(2) > div.css-1dbjc4n.r-yfoy6g.r-z2wwpe.r-xnswec.r-j2cz3j.r-1udh08x.r-u8s1d > div > div > div > a:nth-child(2)',
-        );
-        // @ts-ignore
-        dots?.click();
-      }}
-      children="click"
-    />
-  );
-};
-
-const Button3 = () => {
-  return (
-    <button
-      onClick={async () => {
-        const form = document.querySelector(
-          '#layers > div:nth-child(2) > div > div > div > div > div > div.css-1dbjc4n.r-1awozwy.r-18u37iz.r-1pi2tsx.r-1777fci.r-1xcajam.r-ipm5af.r-g6jmlv > div.css-1dbjc4n.r-1867qdf.r-1wbh5a2.r-kwpbio.r-rsyp9y.r-1pjcn9w.r-1279nm1.r-htvplk.r-1udh08x > div > div > div > div:nth-child(2) > div > div > form',
-          // // '#layers > div:nth-child(2) > div > div > div > div > div > div.css-1dbjc4n.r-1awozwy.r-18u37iz.r-1pi2tsx.r-1777fci.r-1xcajam.r-ipm5af.r-g6jmlv > div.css-1dbjc4n.r-1867qdf.r-1wbh5a2.r-kwpbio.r-rsyp9y.r-1pjcn9w.r-1279nm1.r-htvplk.r-1udh08x > div > div > div > div:nth-child(2) > div.css-1dbjc4n.r-13awgt0.r-eqz5dr.r-1ydw1k6.r-c8eef1.r-bnwqim.r-8fdsdq > div > form > div:nth-child(1) > div > div > label',
-          // // '#layers > div:nth-child(2) > div > div > div > div > div > div.css-1dbjc4n.r-1awozwy.r-18u37iz.r-1pi2tsx.r-1777fci.r-1xcajam.r-ipm5af.r-g6jmlv > div.css-1dbjc4n.r-1867qdf.r-1wbh5a2.r-kwpbio.r-rsyp9y.r-1pjcn9w.r-1279nm1.r-htvplk.r-1udh08x > div > div > div > div:nth-child(2) > div.css-1dbjc4n.r-13awgt0.r-eqz5dr.r-1ydw1k6.r-c8eef1.r-bnwqim.r-8fdsdq > div > form > div:nth-child(1) > div > div > label > div.css-1dbjc4n.r-1wbh5a2A'
-          // '#layers > div:nth-child(2) > div > div > div > div > div > div.css-1dbjc4n.r-1awozwy.r-18u37iz.r-1pi2tsx.r-1777fci.r-1xcajam.r-ipm5af.r-g6jmlv > div.css-1dbjc4n.r-1867qdf.r-1wbh5a2.r-kwpbio.r-rsyp9y.r-1pjcn9w.r-1279nm1.r-htvplk.r-1udh08x > div > div > div > div:nth-child(2) > div > div > form > div:nth-child(1) > div > div > label > div.css-1dbjc4n.r-1wbh5a2 > div.css-1dbjc4n.r-16y2uox > div > span'
-        );
-        // @ts-ignore
-        form?.focus();
-        // form?.chick();
-      }}
-      children="click"
-    />
-  );
-};
-
-const Button14 = () => {
-  return (
-    <button
-      onClick={async () => {
-        const node = document.querySelector(
-          '#layers  > div.css-1dbjc4n.r-1p0dtai.r-1d2f490.r-105ug2t.r-u8s1d.r-zchlnj.r-ipm5af > div > div > div > div:nth-child(2) > div.css-1dbjc4n.r-yfoy6g.r-z2wwpe.r-xnswec.r-j2cz3j.r-1udh08x.r-u8s1d > div > div > div > a:nth-child(2)'
-        );
-        // @ts-ignore
-        item_list?.click();
-      }}
-      children="btn14"
-    />
-  );
-};
-
 // 受信側 other tab -> contents(popup/option -> contents)
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
   console.log(message);
-  const { action } = JSON.parse(message);
-  console.log(action);
-  if (action == 1) {
-    const dots = document.querySelector(
-      '#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div > div.css-1dbjc4n.r-yfoy6g.r-18bvks7.r-1ljd8xs.r-13l2t4g.r-1phboty.r-1jgb5lz.r-11wrixw.r-61z16t.r-1ye8kvj.r-13qz1uu.r-184en5c > div > div:nth-child(2) > div > div > div:nth-child(1) > div > div.css-1dbjc4n.r-1habvwh.r-18u37iz.r-1w6e6rj.r-1wtj0ep > div.css-1dbjc4n.r-obd0qt.r-18u37iz.r-1w6e6rj.r-1h0z5md.r-dnmrzs > div.css-18t94o4.css-1dbjc4n.r-1niwhzg.r-sdzlij.r-1phboty.r-rs99b7.r-6gpygo.r-1kb76zh.r-2yi16.r-1qi8awa.r-1ny4l3l.r-o7ynqc.r-6416eg.r-lrvibr > div'
-    );
-    // @ts-ignore
-    dots?.click();
-  } else if (action == 2) {
-    const item_list = document.querySelector(
-      '#layers > div.css-1dbjc4n.r-1p0dtai.r-1d2f490.r-105ug2t.r-u8s1d.r-zchlnj.r-ipm5af > div > div > div > div:nth-child(2) > div.css-1dbjc4n.r-yfoy6g.r-z2wwpe.r-xnswec.r-j2cz3j.r-1udh08x.r-u8s1d > div > div > div > a:nth-child(2)'
-    );
-    // @ts-ignore
-    item_list?.chick();
+  const msg = JSON.parse(message);
+  if (msg.action == 1) {
+    await addMemberList();
+  } else if (msg.action == 2) {
+    window.location = msg.url;
   }
 });
 
@@ -137,13 +82,7 @@ const main = async () => {
   const BTN: React.FC = () => {
     return (
       <>
-        <Button1 />
-        <Button14 />
-        <Button2 />
-        <Button3 />
-        <button>
-          ok
-        </button>
+        <AddMemberListButton />
       </>
     );
   };
